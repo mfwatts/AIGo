705ffe99b6ba85413673e62e99b8a775b19e6fb4
= <tss_daniel@yahoo.com>
2014-10-08 21:26:08 -0600

    separation between state and output, defined helpful constants

--- board.go
+++ board.go
@@ -3,105 +3,136 @@ package main
 import "fmt"
 import "strconv"
 
-func main() {
-    var dim int =9
-	var board [][]rune = boardSetup(dim)
+//board constants are wrapped ints
+type Piece int
+const(
+  EMPTY = 0
+  WHITE = 1
+  BLACK = 2
+  MARKED_B = 3
+  MARKED_W = 4
+)
 
-	printBoard(board)
-	
-}
+// output unicode constants
+type Tile rune
+const(
+//CORNERS:
+UL = '\u250c'
+LL  = '\u2514'
+UR  = '\u2510'
+LR  = '\u2518'
 
-/*
-CORNERS:
-UL 250c
-LL 2514
-UR 2510
-LR 2518
+//SIDES: 
+R = '\u2524'
+L = '\u251c'
+T = '\u252c'
+B = '\u2534'
+Mid = '\u253c'
 
-SIDES: 
-R 2524
-L 251c
-T 252c
-B - 2534
-Mid - 253c
-WHT - 25cb
-BLK - 25c9
-*/
-func boardSetup(dim int) [][]rune{
+//pieces
+WHT = '\u25cb'
+BLK = '\u25c9'
+)
 
-
-	board := make([][]rune, dim)
+func initBoard(dim int) [][]Piece {
+    board := make([][]Piece, dim)
     for i := range board {
-        board[i] = make([]rune, dim)
+        board[i] = make([]Piece, dim)
     }
-    
-   	for a:=0; a<dim; a++{
-		for b:=0; b<dim;b++{
-			
-			switch{
-				case a == b && b == 0: {
-					board[a][b] ='\u250c' //Upper Left
-				}
-				case a == 0 && b == dim-1:{
-					board[a][b] = '\u2510' //Upper Right
-				}
-				case a == dim-1 && b == 0:{
-					board[a][b] = '\u2514' //Lower Left
-				}
-				case a == b && b == dim-1:{
-					board[a][b] = '\u2518' //Lower Right
-				}
-				case a == 0:{
-					board[a][b] = '\u252c' //Top
-				}
-				case a == dim-1:{
-					board[a][b] = '\u2534' //Bottom
-				}
-				case b == 0:{
-					board[a][b] = '\u251c' //Left
-				}
-				case b == dim-1:{
-					board[a][b] = '\u2524' //Right
-				}
-				default:{
-					board[a][b] = '\u253c' //Middle Tile
-				}
-			}
-		}
-	}
-	return board
-
+    for a:=0; a<dim; a++{
+       for b:=0; b<dim;b++{
+           board[a][b] = EMPTY;
+       }
+    }
+    return board
 }
 
-func printBoard(board [][]rune){
+func printStdBoard(board [][]Piece) {
+
 	var str string = "   "//three spaces
 	var dim  = len(board)
-	alphabet :=[]string{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S"}
-	
+	alphabet :=[]string{"A","B","C","D","E","F","G","H","I","J","K","L",
+                            "M","N","O","P","Q","R","S"}
+
 	for a:= 0; a<dim; a++{
 		str += alphabet[a] +" "
 		}
 
 	fmt.Println(str)
 	str = ""
-	
+
 	for a:=0; a<dim; a++{
 		if(a<10){
 			str += strconv.Itoa(a) +"  "//two spaces
 		}else {
 			str += strconv.Itoa(a) +" ";
 		}
-		
 		for b:=0; b<dim; b++{
-			var straw, _ = strconv.Unquote(strconv.QuoteRune(board[a][b]))
+                        var tile rune = pieceToRune(board[a][b],a,b,dim)
+			var straw, _ = strconv.Unquote(strconv.QuoteRune(tile))
 			str +=straw;
 			if b < dim-1{
 				str+= "\u2500"//horizontal extenders
 			}
-			
+
 	}
 	fmt.Println(str);
 	str = ""
 	}
-	
+
 }
+
+func pieceToRune(piece Piece, a int, b int, dim int) rune {
+    var r rune
+    if piece == EMPTY {
+	switch{
+		case a == b && b == 0: {
+			r ='\u250c' //Upper Left
+		}
+		case a == 0 && b == dim-1:{
+			r = '\u2510' //Upper Right
+		}
+		case a == dim-1 && b == 0:{
+			r = '\u2514' //Lower Left
+		}
+		case a == b && b == dim-1:{
+			r = '\u2518' //Lower Right
+		}
+		case a == 0:{
+			r = '\u252c' //Top
+		}
+		case a == dim-1:{
+			r = '\u2534' //Bottom
+		}
+		case b == 0:{
+			r = '\u251c' //Left
+		}
+		case b == dim-1:{
+			r = '\u2524' //Right
+		}
+		default:{
+			r = '\u253c' //Middle Tile
+		}
+	}
+    } else if piece == WHITE {
+        r = '\u25cb'
+    } else if piece == BLACK {
+        r = '\u25c9'
+    } else if piece == MARKED_W {
+        r = 'W'
+    } else if piece == MARKED_B {
+        r = 'B'
+    }
+    //fmt.Println(piece,strconv.QuoteRune(r))
+    return r
+}
+
+
+func main() {
+    var dim int =9
+	var board [][]Piece = initBoard(dim)
+
+	printStdBoard(board)
+
+}
+
